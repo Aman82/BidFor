e2374a070565551db27417d27c11f8eebb58ea72
Aman82 <jotdhillon@gmail.com>
2014-10-29 17:16:15 -0700

    images does not appear

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
-Paperclip::Attachment.default_options[:url] = 'bidfor.s3.amazonaws.com'
-#Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+# Paperclip::Attachment.default_options[:url] = 'bidfor.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 
-# Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 'bidfor.s3.amazonaws.com'
